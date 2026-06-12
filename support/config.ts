5fc89f453de0bef4851e9398e7b43c7988aa78b8
groverongo <ugartegrover@gmail.com>
2026-06-11 19:34:16 -0500

    hotfix: nuevo default

--- support/config.ts
+++ support/config.ts
@@ -65,7 +65,7 @@ const DEFAULT_CONFIG: Config = {
 		fraccion_extra_campeon: 0.25,
 		factor_bloque_maximo: 10,
 		fecha_inicio_torneo: "2026-06-11T19:00:00.000Z",
-		fecha_cierre_awards: "2026-06-11T21:30:00.000Z",
+		fecha_cierre_awards: "2026-06-12T06:00:00.000Z",
 	},
 };
 
